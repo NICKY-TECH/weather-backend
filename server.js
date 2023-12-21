0709026dde2e01005e0a915b976c428601042388
DevTinaTi <tinadesk8@gmail.com>
2023-12-21 12:11:06 +0100

    add hosted link to cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(cors({
   origin:['http://localhost:5173',
-  "https://weather-frontend-beige.vercel.app/"
+  "https://weather-frontend-beige.vercel.app"
 ]
 }));
 
